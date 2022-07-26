86bb459add1c24d7e1328631e8cde384603d2ed7
Frank Asefuah <asefuahfrank@gmail.com>
2022-07-26 13:24:12 +0000

    Made color and other changes to the site

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public/'))
 
 
 app.get('/', (req, res) => {
