82d3d7fd60a5a7b3e1a7631928e1ac7d86bcbb05
Frank Asefuah <asefuahfrank@gmail.com>
2022-08-11 15:04:49 +0000

    Completed site baring changes

--- server.js
+++ server.js
@@ -2,33 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-// const MongoClient = require('mongodb').MongoClient
-// require('dotenv').config()
-
-
-// let db,
-//     dbConnectionStr = process.env.DB_STRING,
-//     dbName = 'todo'
-
-// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-//     .then(client => {
-//         console.log(`Connected to ${dbName} Database`)
-//         db = client.db(dbName)
-//     })
-
-// app.set('view engine', 'ejs')
 app.use(express.static('public/'))
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
-
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-})
-
-app.get('/network-reg', (req, res) => {
-    res.render('network-reg')
 })
 
 app.listen(PORT || process.env.PORT, () => {
